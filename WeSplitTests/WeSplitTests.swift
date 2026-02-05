dbca028c68152b44a131799a7c8ccce4c7932386
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2026-02-05 21:01:21 +0100

    Test view model calculation logic

--- WeSplitTests/WeSplitTests.swift
+++ WeSplitTests/WeSplitTests.swift
@@ -10,8 +10,20 @@ import Testing
 
 struct WeSplitTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @MainActor
+    @Test func calculatesGrandTotalAndAmountToPay() {
+        let vm = ContentViewModel()
+      
+        #expect(vm.checkAmount == 0)
+        #expect(vm.numberOfPeople == 2)
+        #expect(vm.tipPercent == 20)
+
+        vm.checkAmount = 100
+        vm.numberOfPeople = 3  // actual people = 3 + initial value (2) = 5
+        vm.tipPercent = 25
+
+        #expect(vm.grandTotal == 125.0)
+        #expect(vm.amountEachPersonHasToPay == 25.0)
     }
 
 }
